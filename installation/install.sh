4ddc0e0c1cc5946de038ea0e42a8205c597efcf4
Nick-Eagles <nickeagles77@gmail.com>
2022-09-23 22:12:23 -0400

    Add a script tracking what dependencies are needed to run the code here

---
+++ installation/install.sh
@@ -0,0 +1,7 @@
+#   Create a basic environment with just python
+conda create -y -p $PWD/ML1 python=3.8
+
+#   Add packages we'll need
+conda activate $PWD/ML1
+python -m pip install tensorflow
+conda deactivate
