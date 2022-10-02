4fb214807328f2fa33f88b48651fec97cf106706
Nick-Eagles <nickeagles77@gmail.com>
2022-10-02 14:11:59 -0400

    Add PIL to required packages

--- installation/install.sh
+++ installation/install.sh
@@ -3,5 +3,5 @@ conda create -y -p $PWD/ML1 python=3.8
 
 #   Add packages we'll need
 conda activate $PWD/ML1
-python -m pip install tensorflow
+python -m pip install tensorflow Pillow
 conda deactivate
