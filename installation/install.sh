275911eec56ef3d804f1495de7562bdad7e66a52
Nick-Eagles <nickeagles77@gmail.com>
2025-01-10 18:37:39 -0500

    Don't document how to install things, as the plan will be to have multiple projects and potentially conda environments associated with this repo

--- installation/install.sh
+++
@@ -1,7 +0,0 @@
-#   Create a basic environment with just python
-conda create -y -p $PWD/ML1 python=3.8
-
-#   Add packages we'll need
-conda activate $PWD/ML1
-python -m pip install tensorflow Pillow
-conda deactivate
