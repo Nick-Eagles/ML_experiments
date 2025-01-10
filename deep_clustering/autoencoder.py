46779816f2366b715368a6ef0abb5e511dc1f19b
Nick-Eagles <nickeagles77@gmail.com>
2025-01-10 18:35:30 -0500

    Some architecture changes from a long time ago

--- deep_clustering/autoencoder.py
+++ deep_clustering/autoencoder.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from PIL import Image
+from pyhere import here
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
@@ -13,16 +14,14 @@ y_test = tf.one_hot(y_test, 10, dtype = "float32")
 
 inputs = keras.Input(shape=(28, 28, 1), name="digits")
 
-# #   Encoder
-# x = layers.Conv2D(30, 3, activation="relu", padding="same")(inputs)
-# x = layers.MaxPooling2D(2)(x)
-# x = layers.Conv2D(30, 3, activation="relu", padding="same")(x)
-# x = layers.MaxPooling2D(2)(x)
+################################################################################
+#   Build and compile a convolutional autoencoder
+################################################################################
 
-# #   Decoder
-# x = layers.Conv2DTranspose(30, 3, strides=2, activation="relu", padding="same")(x)
-# x = layers.Conv2DTranspose(30, 3, strides=2, activation="relu", padding="same")(x)
-# x = layers.Conv2D(1, 3, activation="sigmoid", padding="same")(x)
+#   This differs from a traditional convolutional autoencoder in that the
+#   bottleneck layer (encoder output) will be a 1D tensor (vector). Eventually,
+#   the idea will be able to treat each of these "neurons" as a particular
+#   cluster label
 
 #   Encoder
 x = layers.Conv2D(10, 3, activation="relu")(inputs)
@@ -30,17 +29,17 @@ x = layers.Conv2D(10, 3, activation="relu")(x)
 x = layers.MaxPooling2D(2)(x)
 x = layers.Conv2D(10, 3, activation="relu")(x)
 x = layers.Conv2D(10, 3, activation="relu")(x)
-x = layers.GlobalMaxPooling2D()(x)
-encoded = layers.Reshape((1, 1, 10))(x)
+encoded = layers.GlobalMaxPooling2D()(x)
 
 #   Decoder
-x = layers.Conv2DTranspose(30, 2, strides=2, activation="relu")(encoded)
+x = layers.Reshape((1, 1, 10))(x)
+x = layers.Conv2DTranspose(30, 2, strides=2, activation="relu")(x)
 x = layers.Conv2DTranspose(30, 3, strides=3, activation="relu")(x)
 x = layers.Conv2DTranspose(30, 3, strides=5, activation="relu")(x)
-x = layers.Conv2D(1, 3, activation="sigmoid")(x)
+decoded = layers.Conv2D(1, 3, activation="sigmoid")(x)
 
 # Autoencoder
-autoencoder = keras.Model(inputs, x)
+autoencoder = keras.Model(inputs, decoded)
 autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
 autoencoder.summary()
 
